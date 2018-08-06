285d1fcf12e816d47307c582ef50f60059f9279f
dkwyh <261229090@qq.com>
2018-08-06 13:31:59 +0800

    1

---
+++ test_case/test_user.py
@@ -0,0 +1,13 @@
+import unittest, time,os
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.action_chains import ActionChains
+
+import sys
+import test_login
+
+
+
