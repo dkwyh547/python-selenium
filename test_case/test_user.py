dbe7ec1e53ac37272436486ef2a798ec2394abf6
dkwyh <261229090@qq.com>
2018-08-10 16:46:02 +0800

    1

--- test_case/test_user.py
+++ test_case/test_user.py
@@ -1,13 +1,16 @@
-import unittest, time,os
+import unittest,time,os
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoAlertPresentException
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.action_chains import ActionChains
+from modules.modules_test import *
 
-import sys
-import test_login
-
-
-
+driver = webdriver.Firefox()
+driver.implicitly_wait(10)
+driver.get("http://39.106.240.149/")
+HTS_login(drver)
+driver.find_element_by_css_selector(".dropdown-toggle").click()
+driver.find_element_by_css_selector(".dropdown-alerts > li:nth-child(7) > a:nth-child(1)").click()
+driver.find_element_by_link_text("用户管理").click()
+driver.find_element_by_css_selector(".in > li:nth-child(1) > a:nth-child(1)").click()
+driver.switch_to_frame("iframe1")
+driver.find_element_by_id("button_post_insert").click()
